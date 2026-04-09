b51e24c752145913707b90e4bc808638a959e449
Lana Siqueira <helanasiqueira7@gmail.com>
2026-04-08 21:35:44 -0300

    novo commit 1

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -3,9 +3,12 @@ import abahome from "../assets/aba-home.png";
 import abapedidos from "../assets/aba-pedidos.png";
 import abaperfil from "../assets/aba-perfil.png";
 import cart from "../assets/cart.png";
+import dados from "../assets/dados.png";
+import endereco from "../assets/endereco.png";
+import expandir from "../assets/expandir.png";
 import favoritofull from "../assets/favorito-full2.png";
 import favorito from "../assets/favorito.png";
 import logo01 from "../assets/logo01.png";
 import logo02 from "../assets/logo02.png";
 
-export default { logo01, logo02, abahome, abafavorito, abapedidos, abaperfil, cart, favorito, favoritofull };
+export default { logo01, logo02, abahome, abafavorito, abapedidos, abaperfil, cart, favorito, favoritofull, dados, endereco, expandir };
